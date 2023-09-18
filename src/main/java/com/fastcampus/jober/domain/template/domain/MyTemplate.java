769a3bb9cd9a99035979100753f53cbeb0804ef8
SeongHyeon Park <seonghye0n@kakao.com>
2023-09-18 17:09:57 +0900

    feat : MyTemplate Entity 생성
    - 템플릿 모음 찜하기 기능을 위한 Entity
    
    Ref : #3

---
+++ src/main/java/com/fastcampus/jober/domain/template/domain/MyTemplate.java
@@ -0,0 +1,45 @@
+package com.fastcampus.jober.domain.template.domain;
+
+import com.fastcampus.jober.domain.member.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class MyTemplate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany
+    @JoinColumn(name = "template_id")
+    private List<Template> templates;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+}
