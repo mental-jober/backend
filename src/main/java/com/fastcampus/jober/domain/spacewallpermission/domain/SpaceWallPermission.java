3ded7f11543c9cb7ac6ec6f834d55c5c0f4188b0
Park SeongWook <116128609+gosuuk@users.noreply.github.com>
2023-09-20 15:18:17 +0900

    feat: 공유스페이스 권한 Entity 및 Dto 생성

--- src/main/java/com/fastcampus/jober/domain/spacewallpermission/domain/SpaceWallPermission.java
+++ src/main/java/com/fastcampus/jober/domain/spacewallpermission/domain/SpaceWallPermission.java
@@ -1,23 +1,21 @@
 package com.fastcampus.jober.domain.spacewallpermission.domain;
 
-import com.fastcampus.jober.domain.spacewallmember.domain.SpaceWallMember;
+import com.fastcampus.jober.domain.member.domain.Member;
+import com.fastcampus.jober.domain.spacewall.domain.SpaceWall;
 import com.fastcampus.jober.global.constant.Auths;
 import com.fastcampus.jober.global.constant.Type;
 import jakarta.persistence.*;
-import lombok.*;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
-import java.time.LocalDateTime;
-
 @Entity
-@Table(name = "space_wall_permission")
 @Data
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@Builder
 public class SpaceWallPermission {
 
     @Id
@@ -28,8 +26,14 @@ public class SpaceWallPermission {
     //    @JsonIgnore
     @OneToOne
     @OnDelete(action = OnDeleteAction.CASCADE)
-    @JoinColumn(name = "space_wall_member_id")
-    private SpaceWallMember spaceWallMember;
+    @PrimaryKeyJoinColumn
+    private SpaceWall spaceWall;
+
+    //    @JsonIgnore
+    @OneToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @PrimaryKeyJoinColumn
+    private Member member;
 
     @Column(nullable = false)
     @Enumerated(value = EnumType.STRING)
@@ -38,6 +42,9 @@ public class SpaceWallPermission {
     @Column(nullable = false)
     @Enumerated(value = EnumType.STRING)
     private Auths auths; // READ / EDIT / DELETE
+
+    @Column
+    private Long parentId; // 상위 페이지 ID
 
     @Column(nullable = false)
     @CreatedDate
@@ -52,7 +59,10 @@ public class SpaceWallPermission {
         this.createdAt = LocalDateTime.now();
     }
 
-    protected void onUpdate() { // 권한 수정 시 호출
-        this.updatedAt = LocalDateTime.now();
+    @PreUpdate
+    protected void onUpdate() { this.updatedAt = LocalDateTime.now();  }
+
+    public void setAuths(Auths auths) {
+        this.auths = auths;
     }
 }
---
+++ src/main/java/com/fastcampus/jober/domain/spacewallpermission/dto/SpaceWallPermissionResponseDto.java
@@ -0,0 +1,35 @@
+package com.fastcampus.jober.domain.spacewallpermission.dto;
+
+import com.fastcampus.jober.domain.spacewallpermission.domain.SpaceWallPermission;
+import com.fastcampus.jober.global.constant.Auths;
+import com.fastcampus.jober.global.constant.Type;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SpaceWallPermissionResponseDto {
+    private Long id;
+    private Long spaceWallId;  // 공유 스페이스 ID
+    private Long memberId;     // 회원 ID
+    private Type type;         // WHITE / BLACK
+    private Auths auths;       // READ / EDIT / DELETE
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private Long parentId;     // 상위 페이지 ID
+
+    public SpaceWallPermissionResponseDto(SpaceWallPermission spaceWallPermission) {
+        this.id = spaceWallPermission.getId();
+        this.spaceWallId = spaceWallPermission.getSpaceWall().getId();
+        this.memberId = spaceWallPermission.getMember().getId();
+        this.type = spaceWallPermission.getType();
+        this.auths = spaceWallPermission.getAuths();
+        this.createdAt = spaceWallPermission.getCreatedAt();
+        this.updatedAt = spaceWallPermission.getUpdatedAt();
+        this.parentId = spaceWallPermission.getParentId();
+    }
+}
