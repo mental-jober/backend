3d66050a4cfc39317ae9e1652c87b1e2f2ae5f8e
Park SeongWook <116128609+gosuuk@users.noreply.github.com>
2023-09-20 22:33:58 +0900

    Refacator: 공유스페이스 예외 생성

---
+++ src/main/java/com/fastcampus/jober/global/error/exception/SpaceWallBadRequestException.java
@@ -0,0 +1,7 @@
+package com.fastcampus.jober.global.error.exception;
+
+public class SpaceWallBadRequestException extends RuntimeException {
+    public SpaceWallBadRequestException(String message) {
+        super(message);
+    }
+}
