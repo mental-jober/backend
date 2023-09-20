af58f66f38dfb25c4298b6b8a7283e59416aa6e8
Park SeongWook <116128609+gosuuk@users.noreply.github.com>
2023-09-20 22:33:49 +0900

    Refacator: 공유스페이스 dto 수정

---
+++ src/main/java/com/fastcampus/jober/domain/spacewall/dto/SpaceWallRequest.java
@@ -0,0 +1,91 @@
+package com.fastcampus.jober.domain.spacewall.dto;
+
+import com.fastcampus.jober.domain.member.domain.Member;
+import com.fastcampus.jober.domain.spacewall.domain.SpaceWall;
+import com.fastcampus.jober.domain.spacewalllayout.SpaceWallLayout;
+import com.fastcampus.jober.domain.workspace.Workspace;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class SpaceWallRequest {
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CreateDto {
+        private Long layoutId;
+        private Long createMemberId;
+        private Long workspaceId;
+        private String url;
+        private String title;
+        private String description;
+        private String profileImageUrl;
+        private String backgroundImageUrl;
+        private String pathIds;
+        private String shareUrl;
+        private Date shareExpiredAt;
+        private Integer sequence;
+
+        public SpaceWall toEntity() {
+            SpaceWallLayout layout = SpaceWallLayout.builder().id(layoutId).build();
+            Member member = Member.builder().id(createMemberId).build();
+            Workspace workspace = Workspace.builder().id(workspaceId).build();
+
+            LocalDateTime shareExpiration = (shareExpiredAt != null) ?
+                    LocalDateTime.ofInstant(shareExpiredAt.toInstant(), ZoneId.systemDefault()) : null;
+
+            return SpaceWall.builder()
+//                    .layout(layout)
+//                    .createMember(member)
+//                    .workspace(workspace)
+                    .url(url)
+                    .title(title)
+                    .description(description)
+                    .profileImageUrl(profileImageUrl)
+                    .backgroundImageUrl(backgroundImageUrl)
+                    .pathIds(pathIds)
+                    .shareUrl(shareUrl)
+                    .shareExpiredAt(shareExpiration)
+                    .sequence(sequence != null ? sequence : 0)
+                    .build();
+        }
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class UpdateDto {
+        private Long layoutId;
+        private String url;
+        private String title;
+        private String description;
+        private String profileImageUrl;
+        private String backgroundImageUrl;
+        private String pathIds;
+        private String shareUrl;
+        private Date shareExpiredAt;
+
+        public SpaceWall toEntity() {
+            SpaceWallLayout layout = SpaceWallLayout.builder().id(layoutId).build();
+
+            LocalDateTime shareExpiration = (shareExpiredAt != null) ?
+                    LocalDateTime.ofInstant(shareExpiredAt.toInstant(), ZoneId.systemDefault()) : null;
+
+            return SpaceWall.builder()
+//                    .layout(layout)
+                    .url(url)
+                    .title(title)
+                    .description(description)
+                    .profileImageUrl(profileImageUrl)
+                    .backgroundImageUrl(backgroundImageUrl)
+                    .pathIds(pathIds)
+                    .shareUrl(shareUrl)
+                    .shareExpiredAt(shareExpiration)
+                    .build();
+        }
+    }
+}
--- src/main/java/com/fastcampus/jober/domain/spacewall/repository/SpaceWallRepository.java
+++ src/main/java/com/fastcampus/jober/domain/spacewall/repository/SpaceWallRepository.java
@@ -4,6 +4,10 @@ import com.fastcampus.jober.domain.spacewall.domain.SpaceWall;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SpaceWallRepository extends JpaRepository<SpaceWall, Long> {
+
+    Optional<SpaceWall> findByIdAndDeletedAtIsNull(Long id);
 }
