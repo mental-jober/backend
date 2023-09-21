dcfcc16bef5197107ef0f80d7f167f8461579a23
seonghye0n <unknown>
2023-09-22 01:56:39 +0900

    feat : 최근 템플릿 조회 기능 추가
    - 기존 템플릿 모음 조회 API에서 타입이 최근일 때, 최근 사용한 템플릿 조회하는 기능 추가
    
    Ref : #17

--- src/main/java/com/fastcampus/jober/domain/template/service/TemplateService.java
+++ src/main/java/com/fastcampus/jober/domain/template/service/TemplateService.java
@@ -7,17 +7,17 @@ import com.fastcampus.jober.domain.template.dto.TemplateResponseDto.ListDto;
 import com.fastcampus.jober.domain.template.repository.MyTemplateRepository;
 import com.fastcampus.jober.domain.template.repository.TemplateRepository;
 import com.fastcampus.jober.domain.template.repository.TemplateTypeRepository;
+import com.fastcampus.jober.domain.template.repository.TemplateUsedHistoryRepository;
 import com.fastcampus.jober.global.constant.ErrorCode;
 import com.fastcampus.jober.global.constant.TemplateCategory;
 import com.fastcampus.jober.global.error.exception.TemplateException;
 import io.micrometer.common.util.StringUtils;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -26,15 +26,18 @@ public class TemplateService {
     private final TemplateRepository templateRepository;
     private final TemplateTypeRepository templateTypeRepository;
     private final MyTemplateRepository myTemplateRepository;
+    private final TemplateUsedHistoryRepository historyRepository;
 
-    public Page<ListDto> findTemplates(String type, String keyword, int page, int size) {
+    @Transactional(readOnly = true)
+    public Page<ListDto> findTemplates(Member member, String type, String keyword, int page,
+        int size) {
         if (page < 0) {
             throw new TemplateException(ErrorCode.PAGE_BAD_REQUEST);
         }
 
         Pageable pageable = PageRequest.of(page, size);
 
-        Page<Template> templatePage = findTemplatePage(type, keyword, pageable);
+        Page<Template> templatePage = findTemplatePage(member, type, keyword, pageable);
 
         Page<ListDto> listDtos = templatePage.map(m -> ListDto.builder()
             .id(m.getId())
@@ -47,19 +50,18 @@ public class TemplateService {
         return listDtos;
     }
 
+    @Transactional
     public void addTemplate(Member member, Long templateId) {
         Template template = templateRepository.findById(templateId)
             .orElseThrow(() -> new TemplateException(ErrorCode.TEMPLATE_NOT_FOUND));
 
-        List<Template> templates = new ArrayList<>();
-        templates.add(template);
-
-        MyTemplate myTemplate = MyTemplate.builder().member(member).templates(templates).build();
+        MyTemplate myTemplate = MyTemplate.builder().member(member).template(template).build();
 
         myTemplateRepository.save(myTemplate);
     }
 
-    private Page<Template> findTemplatePage(String type, String keyword, Pageable pageable) {
+    private Page<Template> findTemplatePage(Member member, String type, String keyword,
+        Pageable pageable) {
 
         if (StringUtils.isBlank(type) && StringUtils.isBlank(keyword)) {
             return templateRepository.findAll(pageable);
@@ -69,7 +71,12 @@ public class TemplateService {
             return templateRepository.findAllByKeywordContains(keyword, pageable);
         }
 
+        //@TODO: 컨트롤러 단에서 처리하기
         TemplateCategory templateCategory = TemplateCategory.findByName(type);
+
+        if (templateCategory == TemplateCategory.RECENT) {
+            return historyRepository.findAllByMember(member, pageable);
+        }
 
         if (!StringUtils.isBlank(type) && StringUtils.isBlank(keyword)) {
             return templateTypeRepository.findByTemplateCategory(templateCategory, pageable);
