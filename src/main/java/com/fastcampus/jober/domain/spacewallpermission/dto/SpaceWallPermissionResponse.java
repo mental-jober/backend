19976010e5b680b5013a10536a12236b59a857fb
Park SeongWook <116128609+gosuuk@users.noreply.github.com>
2023-09-21 23:46:56 +0900

    refactor: 공유스페이스 권한 기능 수정

--- src/main/java/com/fastcampus/jober/domain/spacewallpermission/dto/SpaceWallPermissionResponse.java
+++ src/main/java/com/fastcampus/jober/domain/spacewallpermission/dto/SpaceWallPermissionResponse.java
@@ -13,8 +13,6 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class SpaceWallPermissionResponse {
     private Long id;
-//    private Long spaceWallId;  // 공유 스페이스 ID
-//    private Long memberId;     // 회원 ID
     private Auths auths;       // READ / EDIT / DELETE
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
@@ -22,8 +20,6 @@ public class SpaceWallPermissionResponse {
 
     public SpaceWallPermissionResponse(SpaceWallPermission spaceWallPermission) {
         this.id = spaceWallPermission.getId();
-//        this.spaceWallId = spaceWallPermission.getSpaceWall().getId();
-//        this.memberId = spaceWallPermission.getMember().getId();
         this.auths = spaceWallPermission.getAuths();
         this.createdAt = spaceWallPermission.getCreatedAt();
         this.updatedAt = spaceWallPermission.getUpdatedAt();
