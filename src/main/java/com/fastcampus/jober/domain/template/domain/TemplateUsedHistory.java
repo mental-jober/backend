679778046704e784593d8bbff9e31c508300fa20
seonghye0n <>
2023-09-21 19:50:29 +0900

    feat : 템플릿 히스토리 등록 메서드 구현
    - TemplateUsedHistory 엔티티 생성
    - TemplateUsedHistoryService에 saveHistory() 메서드 생성
    - 히스토리에 동일한 템플릿이 있다면 사용일 업데이트
    
    Ref : #17

--- src/main/java/com/fastcampus/jober/domain/template/domain/TemplateUsedHistory.java
+++ src/main/java/com/fastcampus/jober/domain/template/domain/TemplateUsedHistory.java
@@ -1,32 +1,44 @@
 package com.fastcampus.jober.domain.template.domain;
 
-import com.fastcampus.jober.domain.BaseTimeEntity;
 import com.fastcampus.jober.domain.member.domain.Member;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
-public class TemplateUsedHistory extends BaseTimeEntity {
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class TemplateUsedHistory {
 
     @Id
-    @Column(nullable = false, unique = true)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @ManyToOne
-    @JoinColumn(name = "template_id")
-    private Template template;
 
     @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
 
-    private LocalDateTime used_at;
+    @ManyToOne
+    @JoinColumn(name = "template_id")
+    private Template template;
 
+    @CreationTimestamp
+    private LocalDateTime usedAt;
+
+    public void updateUsedAt() {
+        this.usedAt = LocalDateTime.now();
+    }
 }
