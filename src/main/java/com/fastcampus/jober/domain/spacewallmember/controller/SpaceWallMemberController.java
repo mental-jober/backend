84451d67625d6296d8a5f788cb3858339dbabcdc
k1m2njun <k1m2njun@naver.com>
2023-09-21 21:21:46 +0900

    Feat: 공유스페이스 공동작업자 관련 API 추가
    - 공유스페이스 공동작업자 추가 및 권한 수정 API (삭제 로직 추가 필요)
    - 공유스페이스 공동작업자 조회 API
    
    Ref: mental-jober#7

--- src/main/java/com/fastcampus/jober/domain/spacewallmember/controller/SpaceWallMemberController.java
+++ src/main/java/com/fastcampus/jober/domain/spacewallmember/controller/SpaceWallMemberController.java
@@ -1,11 +1,12 @@
 package com.fastcampus.jober.domain.spacewallmember.controller;
 
+import com.fastcampus.jober.global.auth.session.SpaceWallContextHolder;
 import com.fastcampus.jober.domain.spacewallmember.dto.SpaceWallMemberRequest;
-import com.fastcampus.jober.domain.spacewallmember.dto.SpaceWallMemberResponse;
 import com.fastcampus.jober.domain.spacewallmember.service.SpaceWallMemberService;
 import com.fastcampus.jober.global.utils.api.dto.ResponseDTO;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,9 +14,18 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/spaces")
 public class SpaceWallMemberController {
 
     private final SpaceWallMemberService spaceWallMemberService;
+
+    /**
+     * 공동작업자 저장 시, 이후
+     * 페이지에 추가되어있는 목록 개수와
+     * 실제 DB에서의 email 개수를 비교하고
+     * 다른 경우
+     * DB의 email들을 추가되어있는 email들에서 검색해보고 없는 경우 삭제.
+     */
 
     /**
      * 공유 멤버 등록 시 객체 리스트를 Json 데이터로 받아 정보를 갱신합니다.
@@ -23,13 +33,11 @@ public class SpaceWallMemberController {
      * @param requests : 공유멤버로 등록하려는 멤버 정보
      * @return : 확인 메세지
      */
-    @PostMapping("/spaces/{spaceWallId}/member")
+    @PostMapping("/member/{spaceWallId}")
     public ResponseEntity<?> spaceWallMemberAdd(
             @PathVariable Long spaceWallId,
             @Valid @RequestBody List<SpaceWallMemberRequest.AssignDTO> requests
     ) {
-        // TODO - API 호출자가 권한을 가지고 있는지 체크
-
         // 멤버 등록 및 권한 수정 로직
         spaceWallMemberService.saveSpaceWallMember(spaceWallId, requests);
 
@@ -41,9 +49,8 @@ public class SpaceWallMemberController {
      * @param spaceWallId : 공유스페이스 id
      * @return : 공유스페이스 멤버 정보
      */
-    @GetMapping("/spaces/{spaceWallId}/member")
+    @GetMapping("/member/{spaceWallId}")
     public ResponseEntity<?> spaceWallMemberList(@PathVariable Long spaceWallId) {
-        // TODO - API 호출자가 권한을 가지고 있는지 체크
 
         return ResponseEntity.ok(
                 new ResponseDTO<>(spaceWallMemberService.findSpaceWallMember(spaceWallId), "공유 멤버를 조회합니다.")
