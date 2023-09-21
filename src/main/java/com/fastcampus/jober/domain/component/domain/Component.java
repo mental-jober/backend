92a1080ef978fe4320f21c5e0a72f3b45e168027
ho30archive <ho30archive@gmail.com>
2023-09-21 19:24:06 +0900

    refactor : 테이블명 수정, 이동
    - spaceWallContent -> Component로 변경
    - spaceWallContentHistory -> ComponentHistory로 변경
    
    Ref: mental-jober#5

--- src/main/java/com/fastcampus/jober/domain/component/domain/Component.java
+++ src/main/java/com/fastcampus/jober/domain/component/domain/Component.java
@@ -1,11 +1,10 @@
-package com.fastcampus.jober.domain.spacewallcontent.domain;
+package com.fastcampus.jober.domain.component.domain;
 
 import com.fastcampus.jober.domain.BaseTimeEntity;
 import com.fastcampus.jober.domain.spacewall.domain.SpaceWall;
 import com.fastcampus.jober.domain.template.domain.Template;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -13,7 +12,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
-public class SpaceWallContent extends BaseTimeEntity {
+public class Component extends BaseTimeEntity {
 
     @Id
     @Column(nullable = false, unique = true)
