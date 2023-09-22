bacf883254be19199b9c3950e10883848306624f
seonghye0n <unknown>
2023-09-22 15:42:28 +0900

    refactor : MyTemplate 엔티티 templates 필드 단일구조로 변경
    - List<Template> templates -> Template template 로 필드 수정
    
    Ref : #17

--- src/main/java/com/fastcampus/jober/domain/template/domain/MyTemplate.java
+++ src/main/java/com/fastcampus/jober/domain/template/domain/MyTemplate.java
@@ -1,16 +1,14 @@
 package com.fastcampus.jober.domain.template.domain;
 
+import com.fastcampus.jober.domain.BaseTimeEntity;
 import com.fastcampus.jober.domain.member.domain.Member;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import com.fastcampus.jober.domain.BaseTimeEntity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -28,9 +26,9 @@ public class MyTemplate extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToMany
+    @ManyToOne
     @JoinColumn(name = "temaplate_id", nullable = false)
-    private List<Template> templates;
+    private Template template;
 
     @ManyToOne
     @JoinColumn(name = "member_id", nullable = false)
