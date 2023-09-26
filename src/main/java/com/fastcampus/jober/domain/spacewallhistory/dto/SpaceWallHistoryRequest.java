8bce7ab57877d3dbc25e3fda7460c5bc8251ffca
k1m2njun <k1m2njun@naver.com>
2023-09-26 16:44:59 +0900

    Feat: 히스토리 관련 Entity, DTO 생성 또는 수정
    - 기존 히스토리 issue 에 있던 커밋은 무시하시면 됩니다.
    
    Ref: mental-jober#2

---
+++ src/main/java/com/fastcampus/jober/domain/spacewallhistory/dto/SpaceWallHistoryRequest.java
@@ -0,0 +1,40 @@
+package com.fastcampus.jober.domain.spacewallhistory.dto;
+
+import com.fastcampus.jober.domain.spacewall.domain.SpaceWall;
+import com.fastcampus.jober.domain.spacewallhistory.domain.SpaceWallHistory;
+
+import java.time.LocalDateTime;
+
+public class SpaceWallHistoryRequest {
+
+    public static class SpaceWallHistorySaveDTO {
+
+        private SpaceWall spaceWall;
+        private String url;
+        private String title;
+        private String description;
+        private String profileImageUrl;
+        private String backgroundImageUrl;
+        private String pathIds;
+        private boolean authorized;
+        private LocalDateTime sharedExpiredAt;
+        private int sequence;
+        private Long createMemberId;
+
+        public SpaceWallHistory toEntity() {
+            return SpaceWallHistory.builder()
+                    .spaceWall(spaceWall)
+                    .url(url)
+                    .title(title)
+                    .description(description)
+                    .profileImageUrl(profileImageUrl)
+                    .backgroundImageUrl(backgroundImageUrl)
+                    .pathIds(pathIds)
+                    .authorized(authorized)
+                    .sharedExpiredAt(sharedExpiredAt)
+                    .sequence(sequence)
+                    .createMemberId(createMemberId)
+                    .build();
+        }
+    }
+}
