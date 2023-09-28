e022dee9939f260a18ef3ae633c4ed091b654600
Park SeongWook <116128609+gosuuk@users.noreply.github.com>
2023-09-28 21:11:38 +0900

    feat: 히스토리 조회 및 불러오기 api 개발

--- src/main/java/com/fastcampus/jober/domain/spacewallhistory/service/SpaceWallHistoryService.java
+++ src/main/java/com/fastcampus/jober/domain/spacewallhistory/service/SpaceWallHistoryService.java
@@ -4,13 +4,17 @@ import com.fastcampus.jober.domain.componenthistory.domain.ComponentHistory;
 import com.fastcampus.jober.domain.componenthistory.dto.ComponentHistoryRequest;
 import com.fastcampus.jober.domain.componenthistory.dto.ComponentHistoryResponse;
 import com.fastcampus.jober.domain.componenthistory.repository.ComponentHistoryRepository;
+import com.fastcampus.jober.domain.member.repository.MemberRepository;
 import com.fastcampus.jober.domain.spacewallhistory.domain.SpaceWallHistory;
 import com.fastcampus.jober.domain.spacewallhistory.dto.HistoryWrapper;
 import com.fastcampus.jober.domain.spacewallhistory.dto.SpaceWallHistoryRequest;
 import com.fastcampus.jober.domain.spacewallhistory.repository.SpaceWallHistoryRepository;
+import com.fastcampus.jober.global.error.exception.SpaceWallBadRequestException;
+import com.fastcampus.jober.global.error.exception.SpaceWallNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +31,7 @@ public class SpaceWallHistoryService {
 
     private final SpaceWallHistoryRepository spaceWallHistoryRepository;
     private final ComponentHistoryRepository componentHistoryRepository;
+    private final MemberRepository memberRepository;
 
     public HistoryResponseDTOWrapper addSpaceWallHistory(HistoryRequestDTOWrapper requestDTO) {
 
@@ -66,5 +71,38 @@ public class SpaceWallHistoryService {
         }
 
         return new HistoryResponseDTOWrapper(spaceWallHistoryResponse, componentHistoriesResponse);
+    }
+    @Transactional(readOnly = true)
+    public List<SpaceWallHistoryResponseDTO> findRecentHistoryByMemberId(Long memberId) {
+        List<SpaceWallHistory> histories = spaceWallHistoryRepository.findTop5ByCreateMemberIdOrderByCreatedAtDesc(memberId);
+        List<SpaceWallHistoryResponseDTO> response = new ArrayList<>();
+
+        for (SpaceWallHistory history : histories) {
+            response.add(new SpaceWallHistoryResponseDTO(history));
+        }
+
+        return response;
+    }
+
+    @Transactional(readOnly = true)
+    public SpaceWallHistoryResponseDTO findHistoryByMemberIdAndHistoryId(Long memberId, Long historyId) {
+        SpaceWallHistory history = spaceWallHistoryRepository.findById(historyId)
+                .orElseThrow(() -> new SpaceWallNotFoundException("히스토리를 찾을 수 없습니다."));
+
+        if (!history.getCreateMemberId().equals(memberId)) {
+            throw new SpaceWallBadRequestException("잘못된 접근입니다.");
+        }
+
+        return new SpaceWallHistoryResponseDTO(history);
+    }
+
+    @Transactional(readOnly = true)
+    public boolean checkMemberIdExists(Long memberId) {
+        return memberRepository.existsById(memberId);
+    }
+
+    @Transactional(readOnly = true)
+    public boolean checkHistoryIdExists(Long historyId) {
+        return spaceWallHistoryRepository.existsById(historyId);
     }
 }
