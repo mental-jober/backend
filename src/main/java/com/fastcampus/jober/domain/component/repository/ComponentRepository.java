c884a42d752f69cdbe206ed67d1f6b45795b980a
Park SeongWook <116128609+gosuuk@users.noreply.github.com>
2023-09-29 23:31:27 +0900

    feat: 공유스페이스 조회 컴퍼넌트 추가

--- src/main/java/com/fastcampus/jober/domain/component/repository/ComponentRepository.java
+++ src/main/java/com/fastcampus/jober/domain/component/repository/ComponentRepository.java
@@ -3,13 +3,14 @@ package com.fastcampus.jober.domain.component.repository;
 import com.fastcampus.jober.domain.component.domain.Component;
 import com.fastcampus.jober.domain.spacewall.domain.SpaceWall;
 import java.util.List;
+
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface ComponentRepository extends JpaRepository<Component, Long> {
 
-
     @Query("select c from Component c where c.spaceWall = :spaceWall")
-    List<Component> findComponentBySpaceWall(SpaceWall spaceWall);
+    List<Component> findComponentBySpaceWall(@Param("spaceWall") SpaceWall spaceWall);
 
 }
