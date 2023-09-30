3519f35fde9a3169984789dda5308219f5a602e0
MebukiYamashi <tschilpen@gmail.com>
2023-10-01 02:32:38 +0900

    global config로 application-prod.yml 참조할 수 있도록 했습니다.

---
+++ src/main/java/com/fastcampus/jober/global/config/AzureStorageProperties.java
@@ -0,0 +1,36 @@
+package com.fastcampus.jober.global.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "cloud.azure.storage")
+public class AzureStorageProperties {
+    private String account;
+    private String key;
+    private String containerName;
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getContainerName() {
+        return containerName;
+    }
+
+    public void setContainerName(String containerName) {
+        this.containerName = containerName;
+    }
+}
