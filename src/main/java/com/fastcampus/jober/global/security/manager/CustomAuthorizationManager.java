cfda1919bfa8721b543abba8046b3d9418be59bc
seonghye0n <>
2023-10-05 15:42:00 +0900

    refactor : 권한 체크 메서드 분리
    - url pattern 별로 메서드 분리함
    - request.getInputStream()은 AuthorizationManager에서 읽을 경우, 컨트롤러에서 바디를 못가져오는 불상사가 일어남
    - HttpServletRequestWrapper를 구현하여 해당 클래스에 request 값을 담음
    - MyRequestFilter에서 해당 request값을 다음 필터로 전달하도록 함
    - AuthorizationFilter 동작 전에 MyRequestFilter가 HttpServletRequestWrapper를 전달함
    
    Ref : #102

--- src/main/java/com/fastcampus/jober/global/security/manager/CustomAuthorizationManager.java
+++ src/main/java/com/fastcampus/jober/global/security/manager/CustomAuthorizationManager.java
@@ -8,8 +8,13 @@ import com.fastcampus.jober.domain.spacewallpermission.repository.SpaceWallPermi
 import com.fastcampus.jober.global.auth.session.MemberDetails;
 import com.fastcampus.jober.global.constant.Auths;
 import com.fastcampus.jober.global.error.exception.SpaceWallNotFoundException;
+import io.micrometer.common.util.StringUtils;
 import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.LinkedHashMap;
 import java.util.function.Supplier;
+import org.apache.tomcat.util.json.JSONParser;
+import org.apache.tomcat.util.json.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authorization.AuthorizationDecision;
@@ -35,32 +40,14 @@ public class CustomAuthorizationManager implements
         RequestAuthorizationContext requestAuthorizationContext) {
         HttpServletRequest request = requestAuthorizationContext.getRequest();
 
-        Long spaceWallId = parseSpaceWallId(request.getRequestURI());
-        SpaceWall spaceWall = spaceWallRepository.findById(spaceWallId)
-            .orElseThrow(() -> new SpaceWallNotFoundException("찾을 수 없습니다."));
-
-        if (request.getRequestURI().contains("/spaces/view") && !spaceWall.isAuthorized()) {
-            return new AuthorizationDecision(true);
-        }
-
         MemberDetails memberDetails = (MemberDetails) authentication.get().getPrincipal();
 
-        SpaceWallMember spaceWallMember = spaceWallMemberRepository.selectSpaceWallMember(
-            spaceWallId, memberDetails.getMemberId());
-
-        if (spaceWallMember == null) {
-            return new AuthorizationDecision(false);
-        }
-
-        Auths auth = permissionRepository.selectAuths(
-            spaceWallMember.getId());
-
-        boolean access = checkSpacesUrlPattern(request, auth);
+        boolean access = checkUrlPattern(request, memberDetails.getMemberId());
 
         return new AuthorizationDecision(access);
     }
 
-    private Long parseSpaceWallId(String requestUri) {
+    private Long parseSpaceWallId(String requestUri) throws NumberFormatException {
         String[] array = requestUri.split("/");
 
         Long spaceWallId = Long.parseLong(array[array.length - 1]);
@@ -68,11 +55,75 @@ public class CustomAuthorizationManager implements
         return spaceWallId;
     }
 
-    private boolean checkSpacesUrlPattern(HttpServletRequest request,
-        Auths auth) {
+    private boolean checkUrlPattern(HttpServletRequest request,
+        Long memberId) {
 
         String uri = request.getRequestURI();
         HttpMethod method = HttpMethod.valueOf(request.getMethod());
+
+        if (uri.contains("/new-spaces")) {
+            return checkNewSpacesUrlPattern(request, memberId);
+        }
+
+        Long spaceWallId = parseSpaceWallId(uri);
+        SpaceWall spaceWall = spaceWallRepository.findById(spaceWallId)
+            .orElseThrow(() -> new SpaceWallNotFoundException("찾을 수 없습니다."));
+
+        if (uri.contains("/spaces/view") && !spaceWall.isAuthorized()) {
+            return true;
+        }
+
+        SpaceWallMember spaceWallMember = spaceWallMemberRepository.selectSpaceWallMember(
+            spaceWallId, memberId);
+
+        if (spaceWallMember == null) {
+            return false;
+        }
+
+        Auths auth = permissionRepository.selectAuths(
+            spaceWallMember.getId());
+
+        if (uri.contains("/spaces")) {
+            return checkSpacesUrlPattern(uri, method, auth);
+        }
+
+        if (uri.contains("Temps")) {
+            return checkTempUrlPattern(uri, method, auth);
+        }
+
+        return false;
+    }
+
+    private boolean checkNewSpacesUrlPattern(HttpServletRequest request, Long memberId) {
+        JSONParser jsonParser = null;
+        try {
+            jsonParser = new JSONParser(request.getInputStream());
+            LinkedHashMap<String, Object> jsonObject = jsonParser.parseObject();
+
+            if (StringUtils.isBlank(jsonObject.get("parentSpaceWallId").toString())) {
+                return true;
+            }
+
+            Long parentSpaceWallId = Long.parseLong(
+                jsonObject.get("parentSpaceWallId").toString());
+
+            SpaceWallMember spaceWallMember = spaceWallMemberRepository.selectSpaceWallMember(
+                parentSpaceWallId, memberId);
+
+            if (spaceWallMember == null) {
+                return false;
+            }
+
+            Auths auth = permissionRepository.selectAuths(
+                spaceWallMember.getId());
+
+            return auth == Auths.EDITOR || auth == Auths.OWNER;
+        } catch (IOException | ParseException e) {
+            return false;
+        }
+    }
+
+    private boolean checkSpacesUrlPattern(String uri, HttpMethod method, Auths auth) {
 
         if (uri.contains("/spaces/member")) {
             return auth == Auths.OWNER;
@@ -93,6 +144,14 @@ public class CustomAuthorizationManager implements
             }
 
             return true;
+        }
+
+        return false;
+    }
+
+    private boolean checkTempUrlPattern(String uri, HttpMethod method, Auths auth) {
+        if (uri.contains("/spaceTemps") || uri.contains("/componentTemps")) {
+            return auth == Auths.EDITOR || auth == Auths.OWNER;
         }
 
         return false;
